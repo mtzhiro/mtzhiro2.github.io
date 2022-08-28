c28a3f8a17c57aa6c56a5f9d210f0df07519c5c4
Hirosato Matsuura <hiromatuura@gmail.com>
2022-08-28 12:26:13 -0300

    Delete testgit.php

--- testgit.php
+++
@@ -1,9 +0,0 @@
-<?php
-$teststr = 'test from php';
-system('cd /var/www/html/githubio/mtzhiro.github.io/');
-system('/usr/bin/git add --all');
-system('/usr/bin/git commit -m "' . $teststr . '"');
-system('/usr/bin/git push origin master');
-/*
-[master 2662f55] test from php Committer: Apache Your name and email address were configured automatically based on your username and hostname. Please check that they are accurate. You can suppress this message by setting them explicitly: git config --global user.name "Your Name" git config --global user.email you@example.com After doing this, you may fix the identity used for this commit with: git commit --amend --reset-author 4 files changed, 32 insertions(+), 8 deletions(-) create mode 100644 testgit.php create mode 100644 testgit.php~
-*/
